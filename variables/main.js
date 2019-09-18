e6628bec17c6fc80d3473d5c97848340f0574ed8
Olisiia Medvedeva <pejma2001@mail.ru>
2019-09-19 00:24:20 +1200

    first commit

---
+++ variables/main.js
@@ -0,0 +1,24 @@
+
+//Задача1
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let averageRate = (algebra + geography + physics) / 3;
+console.log(averageRate);
+
+//Задача2
+
+let name = 'Olisiia';
+let message = `Привет, мир! Меня зовут ${name}`;
+console.log(message);
+
+//Задача3
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
+
